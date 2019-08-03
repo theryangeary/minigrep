7cd118a625f82b3011c7d0c7b57c4ed8ec397dc0
Ryan Geary <rtgnj42@gmail.com>
2019-08-03 13:21:40 -0400

    Read and print file content

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,9 @@ fn main() {
 
     println!("Search for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
