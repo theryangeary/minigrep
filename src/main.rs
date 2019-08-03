d771d5d50a21584d8d04a35161597efb1164e765
Ryan Geary <rtgnj42@gmail.com>
2019-08-03 19:34:09 -0400

    Remove unneeded prints

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Search for {}", config.query);
-    println!("In file {}", config.filename);
-
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
