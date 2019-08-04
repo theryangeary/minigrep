eb2f39e2067b6bdd1f327c5e8d181a336e1ddd5d
Ryan Geary <rtgnj42@gmail.com>
2019-08-04 18:58:55 -0400

    Add insensitive to Config

--- src/lib.rs
+++ src/lib.rs
@@ -3,25 +3,26 @@ use std::env;
 use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
+    let contents = fs::read_to_string(&config.filename[..])?;
 
-    for line in search(&config.query, &contents) {
+    for line in search(&config, &contents) {
         println!("{}", line);
     }
 
     Ok(())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+pub fn search<'a>(config: &Config, contents: &'a str) -> Vec<&'a str> {
     contents
         .lines()
-        .filter(|line| line.contains(query))
+        .filter(|line| line.contains(&config.query))
         .collect()
 }
 
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub insensitive: bool,
 }
 
 impl Config {
@@ -33,8 +34,9 @@ impl Config {
 
         let query = String::from(&args[1]);
         let filename = String::from(&args[2]);
+        let insensitive = false;
 
-        Ok(Config { query, filename })
+        Ok(Config { query, filename, insensitive })
     }
 }
 
@@ -43,16 +45,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
-        let query = "duct";
+    fn case_sensitive() {
+        let config = Config {
+            query: String::from("duct"),
+            filename: String::from(""),
+            insensitive: false,
+        };
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape";
 
         assert_eq!(
             vec!["safe, fast, productive."],
-            search(query, contents)
+            search(&config, contents)
         );
     }
 }
